611f4b18db235e921dae9d8bd20faf62a6e72e3e
Mandaline Teo <mandalineteo@hotmail.com>
2023-09-04 13:30:06 +0800

    Created models and migration files, set up database

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, dependent: :destroy, through: :bookmarks
+
+  validates :name, uniqueness: true, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+movie = Movie.create(
+  title: "Test",
+  overview: "Test",
+  rating: 5
+)
+
+list = List.create(
+  name: "Test",
+)
+
+Bookmark.create(
+  comment: "Test Test Test",
+  movie: movie,
+  list: list
+)
